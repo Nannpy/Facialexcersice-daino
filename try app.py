ee1fb4fdebebfec8c97f74459f0ecdbf02a8c8b9
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-06-23 14:34:34 +0700

    Update try app.py

--- try app.py
+++ try app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import torch
 
+model_path = 'Model.py'
+
+model = torch.load('Model.py')
+model.eval() 
+
+st.title("T-Rex Runner Game")
+
+game_url = "https://vianroyal.github.io/t-rex-runner/"
+st.markdown(
+    f"""
+    <iframe src="{game_url}" width="800" height="600" frameborder="0"></iframe>
+    """,
+    unsafe_allow_html=True
+)
