f616047210ef3c521b93de2f796d23f48bd2e37a
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-06-30 20:07:50 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -9,14 +9,14 @@ import streamlit as st
 from pynput.keyboard import Key,Controller
 from PIL import ImageTk
 
-# try
-st.title('Facial Excersice-DaiNo')   
-game_url = "https://vianroyal.github.io/t-rex-runner/"
-st.markdown(
-    f"""
-    <iframe src="{game_url}" width="800" height="600" frameborder="0"></iframe>
-    """,
-    unsafe_allow_html=True
+# # try
+# st.title('Facial Excersice-DaiNo')   
+# game_url = "https://vianroyal.github.io/t-rex-runner/"
+# st.markdown(
+#     f"""
+#     <iframe src="{game_url}" width="800" height="600" frameborder="0"></iframe>
+#     """,
+#     unsafe_allow_html=True
 )
 
 # Fast Ai
