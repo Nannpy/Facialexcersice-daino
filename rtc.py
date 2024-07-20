292c1d9b6ededcb95109eee593f17375bf24e39a
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-07-20 14:18:05 +0700

    Update rtc.py

--- rtc.py
+++ rtc.py
@@ -1,18 +1,30 @@
-import streamlit as st  
-from streamlit_webrtc import webrtc_streamer
-import cv2
-import av 
+# import streamlit as st  
+# from streamlit_webrtc import webrtc_streamer
+# import cv2
+# import av 
 
-st.title('Daino Game camera Test')
+# st.title('Daino Game camera Test')
 
-# webrtc_streamer(key = 'sample')
+# # webrtc_streamer(key = 'sample')
 
-class VideoProcessor :
-    def recv(self,frame):
-        img = frame.to_ndarray(format = 'bgr24')
+# class VideoProcessor :
+#     def recv(self,frame):
+#         img = frame.to_ndarray(format = 'bgr24')
 
-        img = cv2.cvtColor(cv2.Canny(img,100,200), cv2.COLOR_GRAY2BGR)
+#         img = cv2.cvtColor(cv2.Canny(img,100,200), cv2.COLOR_GRAY2BGR)
 
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+#         return av.VideoFrame.from_ndarray(img, format="bgr24")
     
-webrtc_streamer(key = 'edge', video_processor_factory = VideoProcessor)
+# webrtc_streamer(key = 'edge', video_processor_factory = VideoProcessor)
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import av
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+st.title("Webcam Streamlit App")
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
